b546b4e4bcc732027bd93ec022124d866f73acf3
Adam Gralak <gralak.adam2@wp.pl>
2024-07-07 20:17:39 +0200

    added Form interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => (
-<form className="form">
-          <div>
-            <input className="form__input" type="text"
-              placeholder="Co jest do zrobienia?" />
-          </div>
-          <div className="form__addTask">
-            <button className="form__button">Dodaj zadanie</button>
-          </div>
-        </form>
-);
+const Form = ({addNewTask}) => {
+
+  const [newTaskContent, setNewtaskContent] = useState("");
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    addNewTask(newTaskContent.trim());
+    setNewtaskContent("");
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <div>
+        <input
+          value={newTaskContent}
+          className="form__input"
+          type="text"
+          placeholder="Co jest do zrobienia?"
+          onChange={(e) => setNewtaskContent(e.target.value)}
+        />
+      </div>
+      <div className="form__addTask">
+        <button className="form__button">Dodaj zadanie</button>
+      </div>
+    </form>
+  )
+};
 
 export default Form;
